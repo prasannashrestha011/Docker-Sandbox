a25a1d0f2dedc954f08ebcdac76a124c9fb6aa60
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-05-09 11:40:46 +0545

    initial commit

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"os"
+	"strings"
+
 	sandbox_container "main/actions/container"
 	sandbox_exec "main/actions/exec"
 	sandbox_image "main/actions/image"
@@ -11,9 +16,13 @@ import (
 )
 
 func main() {
-	//docker client
-	//client.FromEnv == reads docker connection string from environment
+	// docker client
+	// client.FromEnv == reads docker connection string from environment
 
+	langCmd := map[string][]string{
+		"python":     {"python", "-c"},
+		"javascript": {"node", "-h"},
+	}
 	ctx := context.Background()
 
 	apiClient, err := sandbox_client.NewSandboxClient()
@@ -21,27 +30,33 @@ func main() {
 		panic(err)
 	}
 	// pull the image
-	imageID := "python:3.11-slim"
+	base := langCmd["javascript"]
+	imageID := sandbox_image.LoadImage("javascript")
 	sandbox_image.PullImage(ctx, apiClient, imageID)
 
-	//creating a container
-
+	// creating a container
 	containerID, err := sandbox_container.CreateContainer(ctx, apiClient, imageID)
 	if err != nil {
 		panic(err)
 	}
-
-	//start the container
-
-	_, err = apiClient.ContainerStart(ctx, containerID, client.ContainerStartOptions{})
-
+	// start the container
 	if err != nil {
 		panic(err)
 	}
 
-	//test commands
-	cmd := []string{"python", "-c", "print(1+2)"}
-	sandbox_exec.ExecCreate(ctx, apiClient, containerID, cmd)
+	// test commands
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter command: ")
+		cmd, _ := reader.ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
+		cmd = strings.ToLower(cmd)
+		if cmd == "exit" {
+			break
+		}
+		dockerCmd := append(base, cmd)
+		sandbox_exec.ExecCreate(ctx, apiClient, containerID, dockerCmd)
+	}
 
 	defer apiClient.ContainerRemove(ctx, containerID, client.ContainerRemoveOptions{
 		Force: true,
